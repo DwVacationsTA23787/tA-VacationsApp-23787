67c8ea53078ef33c098ad8d968e5386dfaa0a96f
Cyb3rWolf945 <59710384+Cyb3rWolf945@users.noreply.github.com>
2024-06-24 23:41:21 +0100

    Addition of No Action instead of Cascade.
    Creation of tables related to the models.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Dw23787.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace Dw23787.Data
 {
+
+    /// <summary>
+    /// Esta classe representa a BD do nosso projeto
+    /// </summary>
     public class ApplicationDbContext : IdentityDbContext
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        /* ********************************************
+        * definir as 'tabelas' da base de dados
+        * ******************************************** */
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Trips> Trips { get; set; }
+        public DbSet<Messages> Messages { get; set; }
+        public DbSet<Groups> Groups { get; set; }
+
     }
 }
