1a85790a6e8d3e0bbe6aec38332d8ccd10b57a5b
Cyb3rWolf945 <59710384+Cyb3rWolf945@users.noreply.github.com>
2024-06-24 21:22:41 +0100

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dw23787.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
